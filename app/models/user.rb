c9b953c7053d2b0cef4524891ccd787e7834244c
ssimonlp <simon@capsens.eu>
2019-04-02 17:24:34 +0200

    sets up user and profile models for devise

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+
+  has_one :profile, dependent: :destroy
+
+  accepts_nested_attributes_for :profile
+
+  validates_associated :profile
+end
