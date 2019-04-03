de176e66a90cd5d85d6aea50a442982ec93747f9
ssimonlp <simon@capsens.eu>
2019-04-03 09:01:45 +0200

    overrides devise controller for nested form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable, :trackable
 
   has_one :profile, dependent: :destroy
 
