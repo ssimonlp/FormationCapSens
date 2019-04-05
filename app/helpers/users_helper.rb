4067f3a42abb14b819bb6214965cb8aeb2114ca9
ssimonlp <simon@capsens.eu>
2019-04-05 11:53:53 +0200

    dry up views

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,9 @@
+module UsersHelper
+  def user_state(user)
+    if user_signed_in?
+      "Bienvenue #{user.profile.first_name} #{user.profile.last_name}"
+    else
+      "Bienvenue visiteur "
+    end
+  end
+end
