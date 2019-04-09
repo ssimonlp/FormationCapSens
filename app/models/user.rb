b502403fc71eb1ccfe037c4738dda78d24bc460e
ssimonlp <simon@capsens.eu>
2019-04-09 11:28:36 +0200

    fix issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,5 @@ class User < ApplicationRecord
   accepts_nested_attributes_for :profile, allow_destroy: true
 
   validates :profile, presence: true
-  validates :password_confirmation, presence: true
   validates_associated :profile
 end
