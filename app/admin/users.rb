50574c1be830e508809f4cea813ecdd1811c2989
ssimonlp <simon@capsens.eu>
2019-04-09 11:48:51 +0200

    add user show

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -64,9 +64,26 @@ ActiveAdmin.register User do
     end
   end
 
+  controller do
+    def updated
+      @user = User.find(params[:id])
+      if params[:user][:password].blank?
+        @user.update_without_password(params[:user])
+      else
+        @user.update_attributes(params[:user])
+      end
+      if @user.errors.blank?
+        redirect_to admin_users_path, :notice => "User updated successfully."
+      else
+        render :edit
+      end
+    end
+  end
+
   form do |f|
     f.inputs do
-      
+      f.input :email
+      f.input :profile_last_name
     end
   end
 end
