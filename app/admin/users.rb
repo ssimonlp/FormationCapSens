9487a457fb350ace30eb7097b7cba03544cd555e
ssimonlp <simon@capsens.eu>
2019-04-09 14:47:26 +0200

    add log as option

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,25 +1,30 @@
-ActiveAdmin.register User do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+# frozen_string_literal: true
 
-  permit_params :email, :password, :password_confirmation, profile_attributes: %i[id first_name last_name date_of_birth]
+ActiveAdmin.register User do
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+
+  permit_params :email,
+                :password,
+                :password_confirmation,
+                profile_attributes: %i[id first_name last_name date_of_birth]
 
   index do
     selectable_column
     id_column
     column :email
     column :last_name do |user|
-       user.profile.last_name
+      user.profile.last_name
     end
     column :first_name do |user|
       user.profile.first_name
@@ -61,6 +66,9 @@ ActiveAdmin.register User do
       row :last_sign_in_ip
       row :created_at
       row :updated_at
+      row :login_as do
+        link_to user.profile.first_name, login_as_admin_user_path(user), target: '_blank', rel: 'noopener'
+      end
     end
   end
 
@@ -70,14 +78,20 @@ ActiveAdmin.register User do
       if params[:user][:password].blank?
         @user.update_without_password(permitted_params[:user])
       else
-        @user.update_attributes(permitted_params[:user])
+        @user.update(permitted_params[:user])
       end
       if @user.errors.blank?
-        redirect_to admin_users_path, :notice => "User updated successfully."
+        redirect_to admin_users_path, notice: "User updated successfully."
       else
         render :edit
       end
     end
+  end
+
+  member_action :login_as, method: :get do
+    @user = User.find(params[:id])
+    bypass_sign_in @user
+    redirect_to my_dashboard_path(@user)
   end
 
   form do |f|
