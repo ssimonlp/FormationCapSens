d49bf6d2c8a49e29e9a829dc7eec5640afd8f9bf
ssimonlp <simon@capsens.eu>
2019-04-11 17:15:57 +0200

    annotate

--- spec/factories/counterparts.rb
+++ spec/factories/counterparts.rb
@@ -5,6 +5,7 @@
 #  id         :bigint(8)        not null, primary key
 #  name       :string
 #  descrition :text
+#  stock      :integer
 #  project_id :bigint(8)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
@@ -12,6 +13,10 @@
 
 FactoryBot.define do
   factory :counterpart do
-    
+    name { Faker::Appliance.unique.equipment }
+    description { Faker::TvShows::SiliconValley.unique.quote }
+    stock { rand(100) + 1 }
+    project_id { rand(Project.first.id..Project.last.id) }
+    user_id { rand(User.first.id..User.last.id) }
   end
 end
--- spec/models/counterpart_spec.rb
+++ spec/models/counterpart_spec.rb
@@ -5,6 +5,7 @@
 #  id         :bigint(8)        not null, primary key
 #  name       :string
 #  descrition :text
+#  stock      :integer
 #  project_id :bigint(8)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
