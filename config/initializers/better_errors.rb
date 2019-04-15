1bc6dcdfb3ca7ba8751392a3c8266ba71b0e8f0a
ssimonlp <simon@capsens.eu>
2019-04-15 14:04:14 +0200

    add better errors gem

---
+++ config/initializers/better_errors.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+if Rails.env.development?
+  module BetterErrorsHugeInspectWarning
+    def inspect_value(obj)
+      inspected = obj.inspect
+      if inspected.size > 100_000
+        inspected = "Object was too large to inspect (#{inspected.size} bytes). " \
+          "Implement #{obj.class}#inspect if you need the details."
+      end
+      CGI.escapeHTML(inspected)
+    rescue NoMethodError
+      "<span class='unsupported'>(object doesn't support inspect)</span>"
+    rescue Exception
+      "<span class='unsupported'>(exception was raised in inspect)</span>"
+    end
+  end
+
+  BetterErrors::ErrorPage.prepend(BetterErrorsHugeInspectWarning)
+end
