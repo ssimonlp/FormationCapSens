c8b5c6faa1d05b196527f8d0f054590b86b994b0
ssimonlp <simon@capsens.eu>
2019-04-17 13:14:57 +0200

    add counterpart transaction

---
+++ app/transactions/counterpart/create_transaction.rb
@@ -0,0 +1,36 @@
+require 'dry/transaction'
+
+class Counterpart::CreateTransaction
+  include Dry::Transaction
+
+  tee :params
+  step :ongoing?
+  step :new
+  tee :save
+
+  def params(input)
+    @params = input.fetch(:params)[:counterpart]
+  end
+
+  def ongoing?(input)
+     @project = Project.find(@params[:project_id])
+     if @project.ongoing? || @project.success? || @project.failure?
+      Failure(input.merge(errors: "You can't add new counterparts to this project."))
+     else
+      Success(input)
+     end
+  end
+  
+  def new(input)
+    @counterpart = Counterpart.new(@params)
+    if @counterpart.valid?
+      Success(input)
+    else
+      Failure(input.merge(errors: @counterpart.errors.messages))
+    end
+  end
+
+  def save(input)
+    @counterpart.save
+  end
+end
