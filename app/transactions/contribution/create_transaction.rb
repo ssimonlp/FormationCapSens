8a61f5e6a896770c3ab81719accd1b626b6563ba
ssimonlp <simon@capsens.eu>
2019-04-23 18:04:54 +0200

    add basic card payin

--- app/transactions/contribution/create_transaction.rb
+++ app/transactions/contribution/create_transaction.rb
@@ -8,6 +8,7 @@ class Contribution::CreateTransaction
   tee :params
   tee :set_value
   step :new
+  step :payin
   tee :save
 
   def params(input)
@@ -26,7 +27,36 @@ class Contribution::CreateTransaction
     end
   end
 
+  def payin(input)
+    begin
+      response = MangoPay::PayIn::Card::Web.create(payin_params.merge(AuthorId: @contribution.user.profile.mangopay_id, DebitedFunds: { Currency: 'EUR', Amount: @contribution.counterpart.price }))
+      if response['Id']
+        Success(input)
+      else
+        Failure(input)
+      end
+      rescue MangoPay::ResponseError => e
+      Failure(input.merge(errors: e.errors.to_a.join(': ')))
+    end
+  end
+
   def save(_input)
     @contribution.save
   end
+
+  protected
+
+  def payin_params
+    @admin = AdminUser.first
+    {
+      CredeitedUserId: 64347949, #Id de l'admin pour l'instant 
+      Fees: { Currency: 'EUR', Amount: 0 }, 
+      CreditedWalletId: 64348317, 
+      CardType: 'CB_VISA_MASTERCARD',
+      ReturnURL: 'https://localhost:300/',
+      Culture: 'FR'
+    }
+  end
 end
+
+
